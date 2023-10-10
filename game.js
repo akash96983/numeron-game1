5c965e3c36d67fac9fef90fb4d04910664870648
Akash9698 <senthilsenthil98832@gmail.com>
2023-10-10 15:53:48 +0530

    done

--- game.js
+++ game.js
@@ -1,5 +1,66 @@
 // Iteration 2: Generate 2 random number and display it on the screen
 
+const number1 = document.getElementById("number1")
+const number2 = document.getElementById("number2")
+
+const buttons = document.getElementById("buttons")
+buttons.addEventListener("click",handleOperations)
+
+const timer = document.getElementById("timer")
+
+// const greater_Than = document.getElementById("greater_than")
+
+// greater_Than.addEvenListener("click",function(){handOperations(">")})
 // Iteration 3: Make the options button functional
 
+displayNumbers()
+function displayNumbers(){
+    num1 = randomNumber()
+    num2 = randomNumber()
+    number1.innerText = num1
+    number2.innerText = num2
+    console.log(num1,num2)
+    }
+    
+    function randomNumber(){
+       return Math.floor(Math.random() *100) + 1
+    }
+
+    let score = 0;
+function handleOperations(e){
+    if(e.target.id=="greater-than" && num1>num2){
+        updateScore()
+    }else if(e.target.id=="equal-to" && num1==num2){
+        updateScore()
+    }else if(e.target.id=="lesser-than" && num1<num2){
+        updateScore()
+    }else{
+        gameover()
+}
+}
+
+function updateScore(){
+    time = 5
+    displayNumbers()
+    score++
+
+}
+
+
+function gameover(){
+    localStorage.setItem("total" , score)
+    location.href = "gameover.html"
+}
 // Iteration 4: Build a timer for the game
+
+setInterval(showTime , 1000)
+
+let time = 5
+function showTime(){
+if(time==0){
+    gameover()
+}else{
+    time--
+    timer.innerText = time;
+}
+}
--- score.js
+++ score.js
@@ -1,1 +1,10 @@
 // Iteration 5: Store the player score and display it on the game over screen
+const score_Board = document.getElementById("score-box")
+
+score_Board.innerText = localStorage.getItem("total")
+
+const play_again_button = document.getElementById("play-again-button")
+
+play_again_button.onclick = function(){
+    location.href = "game.html"
+}
